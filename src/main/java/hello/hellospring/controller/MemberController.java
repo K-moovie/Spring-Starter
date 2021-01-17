7b43611b90ad8637e9e3b9b142d5c8a50bbce8e3
K-moovie <kyh97@naver.com>
2021-01-18 00:30:10 +0900

    jdbc

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,12 +1,17 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 // Spring Container가 해당 객체를 생성한 후 관리
-// @Controller
+@Controller
 public class MemberController {
 
     // Spring Container에 등록하면 해당 객체는 하나만 생성한다.
@@ -21,5 +26,21 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
